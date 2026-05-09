f23e0aa0d269dbec0549e032adaf1c54b6083b0e
VARDHAN <vardhan@VALLURs-MacBook-Air.local>
2026-05-09 21:28:59 +0530

    Add 1,2,3 units notes folder

--- app.js
+++ app.js
@@ -16,7 +16,14 @@ const SUBJECTS = [
                     { name: "DAA Textbook.pdf", path: "DAA/mcq/Design & Analysis of Algorithm  textbook.pdf", size: "2.4 MB" }
                 ]
             },
-
+            {
+                name: "1,2,3 Units Notes",
+                files: [
+                    { name: "DAA Unit 1.pdf", path: "DAA/1,2,3 units notes /DAA_Unit 1.pdf", size: "11.6 MB" },
+                    { name: "DAA Unit 2.pdf", path: "DAA/1,2,3 units notes /DAA_Unit 2.pdf", size: "10.7 MB" },
+                    { name: "DAA Unit 3.pdf", path: "DAA/1,2,3 units notes /DAA_unit3.pdf", size: "5.3 MB" }
+                ]
+            },
             {
                 name: "Written Notes",
                 files: [
